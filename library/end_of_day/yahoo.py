2eced291f68fe6d542681e03bed61c8131173b01
Adam Cataldo <adam.cataldo@gmail.com>
2012-05-15 08:06:56 -0700

    Added Yahoo data feed.

---
+++ library/end_of_day/yahoo.py
@@ -0,0 +1,103 @@
+"""This file contains the Yahoo EndOfDay subclass of the DJ Scrooge Backtesting API.
+Copyright (C) 2012  James Adam Cataldo
+
+    This file is part of Pengoe.
+
+    Pengoe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Pengoe.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from backtest import EndOfDay
+from backtest import Split
+from urllib2 import urlopen
+from datetime import date
+
+class HeadingCsv(object):
+  """A class for parsing Csv files with headings.
+  
+  When iterating over this class, each item is a dictionary, where the keys
+  are headings and the values are 
+  """
+  
+  def __init__(self, file_handle):
+    """Initialize the Object from the given file handle."""
+    self.file_handle = file_handle
+    line = file_handle.readline()
+    if line == '':
+      raise IOError('No heading column found.')
+    self.headings = line.rstrip().split(',')
+   
+  def __iter__(self):
+    """Return self as the iterator."""
+    return self
+  
+  def next(self):
+    """Return the next non-column row and return the corresponding dictionary."""
+    line = self.file_handle.readline()
+    if line == '':
+      raise StopIteration
+    data = line.rstrip().split(',')
+    result = {}
+    for i in range(0, len(data)):
+      result[self.headings[i]] = data[i]
+    return result
+
+class Yahoo(EndOfDay):
+  """An EndOfDay object using Yahoo's REST API."""
+  
+  def __init__(self, symbol, start_date, end_date):
+    """Construct a new Yahoo EndOfDay object."""
+    super(Yahoo, self).__init__(symbol, start_date, end_date)
+    url = 'http://ichart.yahoo.com/table.csv?s=' + symbol
+    url += '&a={0}&b={1}&c={2}'.format(start_date.month - 1, start_date.day, start_date.year)
+    url += '&d={0}&e={1}&f={2}'.format(end_date.month - 1, end_date.day, end_date.year)
+    data = urlopen(url)
+    csv = HeadingCsv(data)
+    for line in csv:
+      dateparts = line['Date'].split('-')
+      self.dates.append(date(int(dateparts[0]), int(dateparts[1]), int(dateparts[2])))
+      self.open_prices.append(int(line['Open'].replace('.', '')))
+      self.high_prices.append(int(line['High'].replace('.', '')))
+      self.low_prices.append(int(line['Low'].replace('.', '')))
+      self.close_prices.append(int(line['Close'].replace('.', '')))
+      self.adj_close_prices.append(int(line['Adj Close'].replace('.', '')))
+      self.volumes.append(int(line['Volume']))
+    url = url.replace('table.csv', 'x')
+    url += '&g=v&y=0'
+    data = urlopen(url)
+    curr_date_index = len(self.dates) - 1
+    self.dividends = [None] * len(self.dates)
+    self.splits = [None] * len(self.dates)
+    for line in data:
+      parts = line.split(',')
+      if parts[0] == 'DIVIDEND':
+        datestr = parts[1].strip()
+        dateobj = date(int(datestr[0:4]), int(datestr[4:6]), int(datestr[6:]))
+        dividend = float(parts[2].strip()) * 100
+        while (self.dates[curr_date_index] < dateobj):
+          curr_date_index -= 1
+        self.dividends[curr_date_index] = dividend
+      elif parts[0] == 'SPLIT':
+        datestr = parts[1].strip()
+        dateobj = date(int(datestr[0:4]), int(datestr[4:6]), int(datestr[6:]))
+        split = parts[2].strip().split(':')
+        while (self.dates[curr_date_index] < dateobj):
+          curr_date_index -= 1
+        self.splits[curr_date_index] = Split(int(split[0]), int(split[1]))
+    self.open_prices.reverse()
+    self.high_prices.reverse()
+    self.low_prices.reverse()
+    self.close_prices.reverse()
+    self.adj_close_prices.reverse()
+    self.dividends.reverse()
+    self.splits.reverse()
---
+++ test/library/end_of_day/test_yahoo.py
@@ -0,0 +1,83 @@
+"""This file contains the XXX of the DJ Scrooge backtesting API.
+Copyright (C) 2012  James Adam Cataldo
+
+    This file is part of Pengoe.
+
+    Pengoe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Pengoe.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from proboscis import test
+from proboscis.asserts import assert_equal
+from proboscis.asserts import assert_true
+from StringIO import StringIO
+from library.end_of_day.yahoo import HeadingCsv, Yahoo
+from datetime import date
+from backtest import Split
+
+@test(groups=['csv'])
+class TestHeadingCsv(object):
+  """Tests for the HeadingCsv class."""
+  
+  @test
+  def testBasic(self):
+    """Test that a simple CSV file object is correctly parsed."""
+    f = StringIO("Foo,Bar,Baz\n1,2,3\n4,5,6")
+    csv = HeadingCsv(f)
+    row = 0
+    for line in csv:
+      assert_equal(len(line), 3)
+      assert_true(line.has_key('Foo'))
+      assert_true(line.has_key('Bar'))
+      assert_true(line.has_key('Baz'))
+      if row == 0:
+        assert_equal(line['Foo'], '1')
+        assert_equal(line['Bar'], '2')
+        assert_equal(line['Baz'], '3')
+      elif row == 1:
+        assert_equal(line['Foo'], '4')
+        assert_equal(line['Bar'], '5')
+        assert_equal(line['Baz'], '6')
+      row += 1
+    assert_equal(row, 2)
+
+@test(depends_on_groups=['csv'])
+class TestYahoo:
+  """Tests the Yahoo EndOfDay class."""
+  
+  @test
+  def test_prices(self):
+    """Test the prices retrieved from Yahoo."""
+    yahoo = Yahoo('GE', date(2012, 4, 27), date(2012, 5, 1))
+    assert_equal(yahoo.open_prices, [1969, 1968, 1958])
+    assert_equal(yahoo.high_prices, [1987, 1972, 1995])
+    assert_equal(yahoo.low_prices, [1960, 1944, 1946])
+    assert_equal(yahoo.close_prices, [1978, 1958, 1980])
+    
+  @test
+  def test_dividends(self):
+    """Test the dividends retrieved from Yahoo."""
+    yahoo = Yahoo('GE', date(2012, 2, 22), date(2012, 2, 24))
+    assert_equal(yahoo.close_prices, [1939, 1931, 1924])
+    assert_equal(yahoo.dividends, [None, 17, None]) 
+
+  @test
+  def test_splits(self):
+    """Test the splits retrieved from Yahoo."""
+    yahoo = Yahoo('GE', date(2000, 5, 5), date(2000, 5, 9))
+    assert_equal(yahoo.close_prices, [15800, 5244, 5213])
+    assert_equal(yahoo.splits, [None, Split(3,1), None]) 
+    
+
+if __name__ == "__main__":
+  from proboscis import TestProgram
+  TestProgram().run_and_exit()
