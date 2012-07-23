56244bf0f14833d225a9d057f638a82d5f35d6d8
Adam Cataldo <adam.cataldo@gmail.com>
2012-07-22 22:27:06 -0700

    Enhanced the suite of techincal analysis functions.

--- djscrooge/technicals.py
+++ djscrooge/technicals.py
@@ -16,26 +16,178 @@ Copyright (C) 2012  James Adam Cataldo
     You should have received a copy of the GNU General Public License
     along with Pengoe.  If not, see <http://www.gnu.org/licenses/>.
 """
+from copy import copy
+from numpy import mean
 
-def simple_moving_average(end_of_day, window):
-  """Calculates a simple moving average of the adjusted close prices.
+def simple_moving_average(values, window):
+  """Calculates a simple moving average of the values.
   
-  end_of_day -- The end_of_day object to calculate the average on.
+  values -- The list of values to calculate the average on.
   window -- The window length of the moving average.
   """
   moving_average = []
-  for i in range(0, len(end_of_day.dates)):
+  for i in range(0, len(values)):
     if i < window:
-      moving_average.append(sum(end_of_day.adj_close_prices[0:(i+1)]) * 1.0 / (i + 1))
+      moving_average.append(sum(values[0:(i+1)]) * 1.0 / (i + 1))
     else:
-      moving_average.append(sum(end_of_day.adj_close_prices[(i+1-window):(i+1)]) * 1.0 / window)
+      moving_average.append(sum(values[(i+1-window):(i+1)]) * 1.0 / window)
   return moving_average
 
-def annulaized_return(start_price, end_price, start_date, end_date):
-  """Computes the annualized return, as a percentage.
+def accumulate(values):
+  """Accumulates values in the series.
   
-  Note that this routine assumes 365.25 days per year, to properly account
-  for leap years.
+  result[i] = sum(values[0:i])
   """
-  years = (end_date - start_date).days / 365.25
-  return ((end_price * 1.0 / start_price) ** (1.0 / years) - 1.0) * 100.0
+  result = copy(values)
+  for i in range(1, len(result)):
+    result[i] += result[i-1]
+  return result
+
+def channel_breakout(values, window):
+  """Calculates channel breakouts.
+  
+  Returns: 0, when there was no breakout, or when the index is less than the window size
+           1, when there was a postive breakout
+          -1, when there was a negative breakout
+  
+  values -- The list of values to calculate the average on.
+  window -- The window length of the moving average.
+  """
+  n = len(values)
+  results = [0] * n
+  for i in range(window, n):
+    value = values[i]
+    sub_array = values[i-window:i]
+    if value > max(sub_array):
+      results[i] = 1
+    elif value < min(sub_array):
+      results[i] = -1
+  return results
+
+def channel_normalization(values, window):
+  """Calculates channel-normalized values.
+  Returns: 50.0, when the value is in the middle of the channel, or for the first index
+           100.0, when at the top of the channel
+           0.0, when at the bottom of the channel
+  
+  values -- The list of values to calculate the average on.
+  window -- The window length of the moving average.  
+  """
+  n = len(values)
+  results = [50.0] * n
+  for i in range(1, window):
+    sub = values[0:i+1]
+    s_min = min(sub)
+    s_max = max(sub)
+    s = values[i]
+    if s_min < s_max:
+      results[i] = 100.0 * (s - s_min) / (s_max - s_min)
+  for i in range(window, n):
+    sub = values[i+1-window:i+1]
+    s_min = min(sub)
+    s_max = max(sub)
+    s = values[i]
+    if s_min < s_max:
+      results[i] = 100.0 * (s - s_min) / (s_max - s_min)
+  return results
+
+def on_balance_volume(prices, volumes):
+  """Returns the on-balance volume for each day.
+  
+  The prices and volumes must be the same length. The on-balance volume
+  is volume times:
+    1 if the price went up from the previous day
+    0 if it staid the same
+    -1 if it went down.
+  """
+  n = len(prices)
+  result = [0] * n
+  for i in range(1, n):
+    if prices[i] > prices[i-1]:
+      result[i] = volumes[i]
+    elif prices[i] < prices[i-1]:
+      result[i] = -volumes[i]
+  return result 
+
+def accumulation_distribution_volume(high_prices, low_prices, close_prices, volumes):
+  """Returns the accumulation/distribution volume (ADV) for each day.
+  
+  The daily range factor is defined as:
+    [(close - low) - (high - close)]/(high - low)
+    
+  The ADV is then the volume times the range factor.
+  """
+  n = len(high_prices)
+  result = [0.0] * n
+  for i in range(0, n):
+    if high_prices[i] > low_prices[i]:
+      range_factor = ((close_prices[i] - low_prices[i]) - (high_prices[i] - close_prices[i])) * 1.0 / (high_prices[i] - low_prices[i])
+    else:
+      range_factor = 0.0
+    result[i] = range_factor * volumes[i]
+  return result
+
+def money_flow(high_prices, low_prices, close_prices, volumes):
+  """Returns the money flow for each day. 
+  
+  This is the ADV multiplied by the average of high, low, and close prices.
+  """
+  result = accumulation_distribution_volume(high_prices, low_prices, close_prices, volumes)
+  n = len(result)
+  for i in range(0, n):
+    result[i] *= mean([high_prices[i], low_prices[i], close_prices[i]])
+  return result
+
+def negative_volume_index(prices, volumes):
+  """Returns the negative volume index for each day.
+  
+  This is the daily return (as a percentage) when the volume is declining, and zero otherwise.
+  """
+  n = len(prices)
+  result = [0.0] * n
+  for i in range(1, n):
+    if volumes[i] < volumes[i-1]:
+      result[i] = (prices[i] * 1.0 / prices[i-1] - 1.0) * 100.0
+  return result
+
+def advance_decline_ratio(advancing_issues, declining_issues, unchanged_issues):
+  """Computes the daily advance/decline ratio.
+  
+  advancing_issues -- The number of issues in the index that advanced since the last day, as a list.
+  declineing_issues -- The number of issues in the index that declined since the last day, as a list.
+  unchanged_issues -- The number of issues in the index that stayed put since the last day, as a list.
+  """
+  n = len(advancing_issues)
+  result = [0.0] * n
+  for i in range(0, n):
+    result[i] = (advancing_issues[i] - declining_issues[i]) * 1.0 / (advancing_issues[i] + declining_issues[i] + unchanged_issues[i])
+  return result
+
+def net_volume_ratio(up_volume, down_volume, unchanged_volume):
+  """Computes the net-volume ratio.
+  
+  up_volume -- The volume advancing issues since the last day, as a list.
+  down_volume -- The volume of declining issues, as a list.
+  unchanged_volume -- The volume of unchanged issues, as a list.
+  """
+  n = len(up_volume)
+  result = [0.0] * n
+  for i in range(0, n):
+    result[i] = (up_volume[i] - down_volume[i]) * 1.0 / (up_volume[i] + down_volume[i] + unchanged_volume[i])
+  return result
+
+def high_low_ratio(advancing_issues, declining_issues, unchanged_issues, new_highs, new_lows):
+  """Computes the daily high/low ratio.
+  
+  advancing_issues -- The number of issues in the index that advanced since the last day, as a list.
+  declineing_issues -- The number of issues in the index that declined since the last day, as a list.
+  unchanged_issues -- The number of issues in the index that stayed put since the last day, as a list.
+  new_highs -- The number of new highs of each day, over some interval (often 52 weeks)
+  new_lows -- The number of new lows of each day, over the same interval
+  """
+  n = len(advancing_issues)
+  result = [0.0] * n
+  for i in range(0, n):
+    result[i] = (new_highs[i] - new_lows[i]) * 1.0 / (advancing_issues[i] + declining_issues[i] + unchanged_issues[i])
+  return result
+
--- djscrooge/test/test_technicals.py
+++ djscrooge/test/test_technicals.py
@@ -18,32 +18,118 @@ Copyright (C) 2012  James Adam Cataldo
 """
 from proboscis import test
 from proboscis.asserts import assert_equal
-from djscrooge.technicals import simple_moving_average, annulaized_return
-from djscrooge.backtest import EndOfDay
-from datetime import date, timedelta
+from djscrooge.technicals import simple_moving_average, accumulate,\
+  channel_breakout, channel_normalization, on_balance_volume,\
+  accumulation_distribution_volume, money_flow, negative_volume_index,\
+  advance_decline_ratio, net_volume_ratio, high_low_ratio
 
 @test
 def test_simple_moving_average():
   """Test the simple_moving_average function."""
-  end_of_day = EndOfDay('FOO', date(2000,1,1), date(2000,1,1) + timedelta(5))
-  end_of_day.dates = []
-  end_of_day.dates.append(date(2000,1,1))
-  end_of_day.adj_close_prices.append(0)
-  for i in range(1, 6):
-    end_of_day.dates.append(end_of_day.dates[i - 1] + timedelta(1))
-    end_of_day.adj_close_prices.append(i)
+  
   expected = [0.0, 0.5, 1.5, 2.5, 3.5, 4.5]
-  actual = simple_moving_average(end_of_day, 2)
+  actual = simple_moving_average(range(0, 6), 2)
   assert_equal(actual, expected)
   
 @test
-def test_annualized_return():
-  """Test the annualized_return function."""
-  start = date(2001, 1, 1)
-  end = date(2005, 1, 1)
-  actual = annulaized_return(1, 16, start, end)
-  assert_equal(actual, 100.0)
+def test_accumulate():
+  """Test the accumulate function."""
+  values = [1, 2, 3, 4]
+  actual = accumulate(values)
+  expected = [1, 3, 6, 10]
+  assert_equal(actual, expected)
+    
+@test
+def test_channel_breakout():
+  """Test the channel_breakout function."""
+  values = [0, 1, 4, 2, 3, 1, 6, 0]
+  window = 2
+  actual = channel_breakout(values, window)
+  expected = [0, 0, 1, 0, 0, -1, 1, -1]
+  assert_equal(actual, expected)
   
+@test
+def test_channel_normalization():
+  """Test the channel_normalization function."""
+  values = [0, 1, 4, 2, 3, 1, 6, 0]
+  window = 3
+  actual = channel_normalization(values, window)
+  expected = [50.0, 100.0, 100.0, 100.0/3.0, 50.0, 0.0, 100.0, 0.0]
+  assert_equal(actual, expected)
+  
+@test
+def test_on_balance_volume():
+  """Tets the on_balance_volume function."""
+  prices = [1, 2, 2, 1]
+  volumes = [1, 2, 3, 4]
+  actual = on_balance_volume(prices, volumes)
+  expected = [0, 2, 0, -4]
+  assert_equal(actual, expected)
+  
+@test
+def test_accumulation_distribution_volume():
+  """Test the accumulation_distribution_volume function."""
+  high_prices = [4, 4, 4, 4, 4]
+  low_prices = [0, 0, 0, 0, 0]
+  close_prices = [2, 1, 3, 0, 4]
+  volumes = [1, 1, 1, 1, 1]
+  actual = accumulation_distribution_volume(high_prices, low_prices, close_prices, volumes)
+  expected = [0.0, -0.5, 0.5, -1.0, 1.0]
+  assert_equal(actual, expected)  
+
+@test
+def test_money_flow():
+  """Test the money_flow function."""
+  high_prices = [4, 4, 4, 4, 4]
+  low_prices = [0, 0, 0, 0, 0]
+  close_prices = [2, 1, 3, 0, 4]
+  volumes = [1, 1, 1, 1, 1]
+  actual = money_flow(high_prices, low_prices, close_prices, volumes)
+  expected = [0.0, -0.5 * (5.0/3.0), 0.5 * (7.0/3.0), -1.0 * (4.0/3.0), 1.0 * (8.0/3.0)]
+  assert_equal(actual, expected)
+  
+@test
+def test_negative_volume_index():
+  """Test the negative_volume_index function."""
+  prices = [1, 2, 4, 8]
+  volumes = [1, 2, 1, 2]
+  actual = negative_volume_index(prices, volumes)
+  expected = [0.0, 0.0, 100.0, 0.0]  
+  assert_equal(actual, expected)
+  
+@test
+def test_advance_decline_ratio():
+  """Test the advance_decline_ratio function."""
+  advancing_issues = [1, 2, 3]
+  declining_issues = [3, 2, 1]
+  unchanged_issues = [1, 0, 2]
+  actual = advance_decline_ratio(advancing_issues, declining_issues, unchanged_issues)
+  expected = [-0.4, 0.0, 1.0 / 3.0]
+  assert_equal(actual, expected)
+
+@test
+def test_net_volume_ratio():
+  """Test the net_volume_ratio function."""
+  up_volume = [1, 2, 3]
+  down_volume = [3, 2, 1]
+  unchanged_volume = [1, 0, 2]
+  actual = net_volume_ratio(up_volume, down_volume, unchanged_volume)
+  expected = [-0.4, 0.0, 1.0 / 3.0]
+  assert_equal(actual, expected)
+
+@test
+def test_high_low_ratio():
+  """Test the high_low_ratio function."""
+  advancing_issues = [1, 2, 3]
+  declining_issues = [3, 2, 1]
+  unchanged_issues = [1, 0, 2]
+  new_highs = [1, 2, 3]
+  new_lows = [3, 2, 1]
+  actual = high_low_ratio(advancing_issues, declining_issues, unchanged_issues, new_highs, new_lows)
+  expected = [-0.4, 0.0, 1.0 / 3.0]
+  assert_equal(actual, expected)
+
+
 if __name__ == "__main__":
   from proboscis import TestProgram
   TestProgram().run_and_exit()
